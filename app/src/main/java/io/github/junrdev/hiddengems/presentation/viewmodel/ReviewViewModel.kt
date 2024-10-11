c40ce0498a9a8c751ea216aa75ec9c6d29c0e8ed
kidiga <juniorprogrammer09@gmail.com>
2024-10-11 22:10:31 +0300

    reviews viewmodel impl

---
+++ app/src/main/java/io/github/junrdev/hiddengems/presentation/viewmodel/ReviewViewModel.kt
@@ -0,0 +1,51 @@
+package io.github.junrdev.hiddengems.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.junrdev.hiddengems.data.model.Review
+import io.github.junrdev.hiddengems.data.repo.ReviewsRepo
+import io.github.junrdev.hiddengems.util.Resource
+import javax.inject.Inject
+
+@HiltViewModel
+class ReviewViewModel @Inject constructor(
+    private val reviewsRepo: ReviewsRepo
+) : ViewModel() {
+
+
+    private val _allreviews: MutableLiveData<Resource<List<Review>>> = MutableLiveData()
+    val allreviews: LiveData<Resource<List<Review>>> get() = _allreviews
+
+
+    private val _gemreviews: MutableLiveData<Resource<List<Review>>> = MutableLiveData()
+    val gemreviews: LiveData<Resource<List<Review>>> get() = _allreviews
+
+
+    suspend fun addReview(review: Review, onResource: (Resource<String>) -> Unit) {
+        reviewsRepo.addReview(review) {
+            onResource(it)
+        }
+    }
+
+
+    suspend fun getAllReviews() {
+        reviewsRepo.getAllReviews {
+            _allreviews.postValue(it)
+        }
+    }
+
+    suspend fun getGemReviews(gemId: String) {
+        reviewsRepo.getGemReviews(gemId) {
+            _gemreviews.postValue(it)
+        }
+    }
+
+    suspend fun getReviewById(reviewId: String, onResource: (Resource<Review>) -> Unit) {
+        reviewsRepo.getReviewById(reviewId) {
+            onResource(it)
+        }
+    }
+
+}
