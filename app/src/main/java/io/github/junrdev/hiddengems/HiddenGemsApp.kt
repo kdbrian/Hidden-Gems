7640d1c9a9128edd15837efaf39dad2256bdba51
kidiga <juniorprogrammer09@gmail.com>
2024-10-14 13:57:23 +0300

    plant debug  tree timber

--- app/src/main/java/io/github/junrdev/hiddengems/HiddenGemsApp.kt
+++ app/src/main/java/io/github/junrdev/hiddengems/HiddenGemsApp.kt
@@ -6,6 +6,12 @@ import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.cache.DiskCache
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class HiddenGemsApp : Application()
+class HiddenGemsApp : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
