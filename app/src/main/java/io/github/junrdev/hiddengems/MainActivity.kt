bfacbecb4f7ceddbfc21ff896f8a49e2717a619f
briank <98683954+kdbrian@users.noreply.github.com>
2025-06-28 23:44:46 +0300

    Update MainActivity.kt

--- app/src/main/java/io/github/junrdev/hiddengems/MainActivity.kt
+++ app/src/main/java/io/github/junrdev/hiddengems/MainActivity.kt
@@ -2,51 +2,27 @@ package io.github.junrdev.hiddengems
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
-import dagger.hilt.android.AndroidEntryPoint
-import io.github.junrdev.hiddengems.databinding.ActivityMainBinding
-import io.github.junrdev.hiddengems.presentation.ui.AppDatastore
-import io.github.junrdev.hiddengems.presentation.ui.showToast
-import io.github.junrdev.hiddengems.presentation.viewmodel.UsersViewModel
-import io.github.junrdev.hiddengems.util.Resource
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.FormBody
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import org.json.JSONObject
-import java.io.IOException
-import javax.inject.Inject
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import cafe.adriel.voyager.navigator.Navigator
+import com.jakeoils.android.presentation.ui.theme.HiddenGemsTheme
+import timber.log.Timber
 
-@AndroidEntryPoint
+
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var appDatastore: AppDatastore
-
-    lateinit var binding: ActivityMainBinding
-    private val usersViewModel by viewModels<UsersViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         handleGithubAouth(intent)
+        setContent {
+            HiddenGemsTheme{
+            }
+        }
     }
 
 
@@ -67,37 +43,7 @@ class MainActivity : AppCompatActivity() {
                     CoroutineScope(Dispatchers.Main).launch {
                         //login with code -> save data if not -> save uid -> proceed
                         val tokenResource = usersViewModel.loginGithubUserWithCode(code)
-                        when (tokenResource) {
-                            is Resource.Error -> applicationContext.showToast(tokenResource.message.toString())
-                            is Resource.Loading -> Unit
-                            is Resource.Success -> {
-                                //we have the token
-                                usersViewModel.saveGithubUser(tokenResource.data!!) { userIdResource ->
-                                    when (userIdResource) {
-                                        is Resource.Error -> applicationContext.showToast(
-                                            userIdResource.message.toString()
-                                        )
-
-                                        is Resource.Loading -> Unit
-
-                                        is Resource.Success -> {
-                                            //user viewmodel to save user info
-                                            CoroutineScope(Dispatchers.Main).launch {
-                                                appDatastore.loginGhubUser(
-                                                    tokenResource.data,
-                                                    userIdResource.data!!
-                                                )
-                                                val navController =
-                                                    binding.fragmentContainerView.findNavController()
-                                                navController.navigate(R.id.homeScreen)
-
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-
-                        }
+                    
                     }
                 }
             }
@@ -140,11 +86,7 @@ class MainActivity : AppCompatActivity() {
 
                             val token = json.getString("access_token")
                             token.let {
-//                                appDatastore.loginGhubUser(token)
-                                //user viewmodel to save user info
-                                val navController =
-                                    binding.fragmentContainerView.findNavController()
-                                navController.navigate(R.id.appnavigation)
+//                                
                             }
                         }
                     }
