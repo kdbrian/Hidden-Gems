5f2da515fb565f84ab20f4ce9f9aa3bed48a9836
briank <98683954+kdbrian@users.noreply.github.com>
2025-06-28 23:57:56 +0300

    Update HiddenGemsApp.kt

--- app/src/main/java/io/github/junrdev/hiddengems/HiddenGemsApp.kt
+++ app/src/main/java/io/github/junrdev/hiddengems/HiddenGemsApp.kt
@@ -5,10 +5,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.cache.DiskCache
-import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
-@HiltAndroidApp
 class HiddenGemsApp : Application(){
     override fun onCreate() {
         super.onCreate()
