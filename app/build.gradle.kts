aaf2831782da23591230ed04f3e3b02ace1d0c4a
briank <98683954+kdbrian@users.noreply.github.com>
2025-06-28 23:22:34 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
+   alias(libs.plugins.kotlin.compose) alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 
-    id("kotlin-kapt")
-    id("kotlin-parcelize")
-
-    id("com.google.dagger.hilt.android")
+    kotlin("plugin.serialization") version "2.1.20"
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    alias(libs.plugins.google.gms.google.services)
+    alias(libs.plugins.google.firebase.crashlytics)
 
     //proto
 //    id("com.google.protobuf") version "0.8.17"
@@ -51,94 +51,97 @@ android {
         jvmTarget = '1.8'
     }
     buildFeatures {
-        //...
+        buildConfig = true
         compose true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion '1.3.2'
-    }
+    
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.analytics)
+    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    //auth
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.auth)
-
-    //firestore
-    implementation(libs.firebase.firestore)
-
-    //maps
-    implementation(libs.play.services.maps)
-    implementation(libs.play.services.maps.v1810)
-    implementation(libs.play.services.location)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 
 
-    //nav
-    implementation(libs.androidx.navigation.fragment)
-    implementation(libs.androidx.navigation.ui)
 
-    //storage
-    implementation(libs.firebase.storage)
-    testImplementation(libs.junit)
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
 
-    //room
-    implementation(libs.androidx.room.ktx)
-    kapt(libs.androidx.room.compiler)
+    val voyagerVersion = "1.1.0-beta02"
+    // Screen Model
+    implementation("cafe.adriel.voyager:voyager-screenmodel:$voyagerVersion")
+    // BottomSheetNavigator
+    implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion")
+    // TabNavigator
+    implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion")
 
-    //circle image view
-    implementation(libs.circleimageview)
+    // Transitions
+    implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion")
 
-    //glide
-    implementation(libs.glide)
+    // Koin integration
+    implementation("cafe.adriel.voyager:voyager-koin:$voyagerVersion")
 
-    //hilt
-    implementation(libs.hilt.android)
-    kapt(libs.hilt.android.compiler)
-
-    //shimmer
-    implementation(libs.shimmer)
-
-    //datastore
-    implementation(libs.androidx.datastore.preferences)
+    // LiveData integration
+    implementation("cafe.adriel.voyager:voyager-livedata:$voyagerVersion")
 
 
-    //gson
-    implementation(libs.gson)
+    //ktor
+    val ktor_version = "3.1.3"
+    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
 
-    //okhttp
-    implementation(libs.okhttp)
-
-    //protobuf
-//    implementation(libs.protobuf.javalite)
-
-    //lottie
-//    implementation(libs.lottie)
-
-    implementation(libs.androidx.swiperefreshlayout)
-
-    //chrome browser tabs extension
-    implementation(libs.androidx.browser)
-
-    //timber-> logger
-    implementation(libs.timber)
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
 
-    //gen ai
-    implementation("com.google.ai.client.generativeai:generativeai:0.7.0")
+    val koin_version = "4.0.3"
+    implementation(platform("io.insert-koin:koin-bom:$koin_version"))
+    implementation("io.insert-koin:koin-core")
+    implementation("io.insert-koin:koin-android")
+    implementation("io.insert-koin:koin-compose")
+    implementation("io.insert-koin:koin-compose-viewmodel")
 
-}
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
-// Allow references to generated code
-kapt {
-    correctErrorTypes = true
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.1")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
+
+    implementation("com.google.accompanist:accompanist-permissions:0.37.3")
+
+
+    //camera
+    val camerax_version = "1.2.2"
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-video:${camerax_version}")
+
+    implementation("androidx.camera:camera-view:${camerax_version}")
+    implementation("androidx.camera:camera-extensions:${camerax_version}")
+
+
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation ("io.github.ehsannarmani:compose-charts:0.1.7")
+
 }
 
