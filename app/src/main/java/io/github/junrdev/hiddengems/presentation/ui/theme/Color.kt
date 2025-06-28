aba2a01f5f7558fcad9d5f0d00a5e8cf2fd2789c
briank <98683954+kdbrian@users.noreply.github.com>
2025-06-28 23:35:07 +0300

    Create Color.kt

---
+++ app/src/main/java/io/github/junrdev/hiddengems/presentation/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package io.github.junrdev.hiddengems.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val chillRed = Color("#FFD84727")
+  val richBlack=("#FF071013")
+    val davysGrey =Color("#FF535657")
+    val aliceBlue=("#FFF4FAFF")
